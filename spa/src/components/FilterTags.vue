813ff4e8b5036b9c1ef00e3b9f2c4e731de8ffe5
trin <trin4ik@gmail.com>
2025-06-27 12:43:08 +0300

    highlight active tag/category

--- spa/src/components/FilterTags.vue
+++ spa/src/components/FilterTags.vue
@@ -21,7 +21,7 @@
           :key="getTagValue(tag)"
           :label="getTagLabel(tag)"
           :value="getTagValue(tag)"
-          :is-active="activeTags.includes(tag)"
+          :is-active="!!activeTags.filter(t => t.value === tag.value).length"
           @click="handleTagClick"
         />
       </div>
